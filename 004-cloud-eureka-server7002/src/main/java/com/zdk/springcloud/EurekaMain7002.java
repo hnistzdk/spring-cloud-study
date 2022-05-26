c7a07403324a51bcc9f3b0b5bd9756a1e4d9d64d
hnist <369365576@qq.com>
2022-05-26 21:23:52 +0800

    eureka单机、集群

--- 004-cloud-eureka-server7002/src/main/java/com/zdk/springcloud/EurekaMain7002.java
+++ 004-cloud-eureka-server7002/src/main/java/com/zdk/springcloud/EurekaMain7002.java
@@ -2,16 +2,16 @@ package com.zdk.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author zdk
- * @date 2022/5/25 19:45
+ * @date 2022/5/26 20:24
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class);
+        SpringApplication.run(EurekaMain7002.class);
     }
 }
